20662780caf49080ec27c77384fa55d70300de60
Dilip Kumar <curiousdilip@gmail.com>
2023-04-26 12:33:15 +0530

    movie format

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { onMounted, ref } from "vue";
+import movies from './movie.json'
 import Magnify from "vue-material-design-icons/Magnify.vue";
 import HomeOutline from "vue-material-design-icons/HomeOutline.vue";
 import TrendingUp from "vue-material-design-icons/TrendingUp.vue";
@@ -7,36 +8,54 @@ import Television from "vue-material-design-icons/Television.vue";
 import MovieOutline from "vue-material-design-icons/MovieOutline.vue";
 import Plus from "vue-material-design-icons/Plus.vue";
 import ChevronLeft from "vue-material-design-icons/ChevronLeft.vue";
-import {useMovieStore} from './stores/movie'
+
+import MovieDetails from "./components/MovieDetails.vue";
+import { useMovieStore } from './stores/movie'
 import { storeToRefs } from "pinia";
-const useMovie=useMovieStore()
-const {movie, showFullVideo}=storeToRefs(useMovie)
+const useMovie = useMovieStore()
+const { movie, showFullVideo } = storeToRefs(useMovie)
+
+onMounted(() => {
+  setTimeout(() => movie.value = movies[0][0], 100)
+})
 </script>
 
 <template>
   <div class="fixed w-full h-screen bg-black">
     <div v-if="!showFullVideo" id="SideNav" class="flex z-40 items-center w-[120px] h-screen bg-black relative">
-    <img src="../public/images/netflix-logo.png" alt="" class="absolute top-0 w-[35px] mt-10 ml-10">
-    <div class="">
-      <div class="py-2 mx-10 my-6">
-        <Magnify fillColor="#fff" :size=40 class="cursor-pointer"/>
+      <img src="../public/images/netflix-logo.png" alt="" class="absolute top-0 w-[35px] mt-10 ml-10">
+      <div class="">
+        <div class="py-2 mx-10 my-6">
+          <Magnify fillColor="#fff" :size=40 class="cursor-pointer" />
+        </div>
+        <div class="py-2 mx-10 my-6 border-b-4 border-b-red-500">
+          <HomeOutline fillColor="#fff" :size=40 class="cursor-pointer" />
+        </div>
+        <div class="py-2 mx-10 my-6">
+          <TrendingUp fillColor="#fff" :size=40 class="cursor-pointer" />
+        </div>
+        <div class="py-2 mx-10 my-6">
+          <Television fillColor="#fff" :size=40 class="cursor-pointer" />
+        </div>
+        <div class="py-2 mx-10 my-6">
+          <MovieOutline fillColor="#fff" :size=40 class="cursor-pointer" />
+        </div>
+        <div class="py-2 mx-10 my-6">
+          <Plus fillColor="#fff" :size=40 class="cursor-pointer" />
+        </div>
       </div>
-      <div class="py-2 mx-10 my-6 border-b-4 border-b-red-500">
-        <HomeOutline fillColor="#fff" :size=40 class="cursor-pointer"/>
-      </div>
-      <div class="py-2 mx-10 my-6">
-        <TrendingUp fillColor="#fff" :size=40 class="cursor-pointer"/>
-      </div><div class="py-2 mx-10 my-6">
-        <Television fillColor="#fff" :size=40 class="cursor-pointer"/>
-      </div><div class="py-2 mx-10 my-6">
-        <MovieOutline fillColor="#fff" :size=40 class="cursor-pointer"/>
-      </div><div class="py-2 mx-10 my-6">
-        <Plus fillColor="#fff" :size=40 class="cursor-pointer"/>
+
+      <div class="" v-if="!showFullVideo">
+        <div class="fixed flex z-20 top-0 right-0 w-full h-[50%] bg-black pl-[120px] bg-clip-border">
+
+          <MovieDetails v-if="movie" :movie="movie" />
+
+          <video v-if="movie" :src="'../public/videos/' + movie.name + '.mp4'" autoplay loop
+            class="absolute z-0 h-[600px] right-0 top-0" />
+        </div>
       </div>
     </div>
   </div>
-  </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
---
+++ src/components/MovieDetails.vue
@@ -0,0 +1,27 @@
+<script setup>
+defineProps({
+    movie: Objects
+})
+</script>
+
+<template>
+    <div v-if="movie" class="z-40 text-white w-[53%] pt-20">
+        <div class="text-[55px] font-semibold font-serif">{{ movie.name }}</div>
+    </div>
+    <div class="flex item-center text-lg pt-4">
+        <div class="">{{ movie.year }}</div>
+        <div class="relative">
+            <span class="absolute left-[2px] -top-[12px] text-[40px]">.</span>
+            <span class="pl-4">{{ movie.genre.one }}</span>
+        </div>
+        <div class="relative">
+            <span class="absolute left-[2px] -top-[12px] text-[40px]">.</span>
+            <span class="pl-4">{{ movie.genre.two }}</span>
+        </div>
+        <div class="relative">
+            <span class="absolute left-[2px] -top-[12px] text-[40px]">.</span>
+            <span class="pl-4">{{ movie.genre.three }}</span>
+        </div>
+        <div class="text-2xl pt-6">{{ movie.description }}</div>
+    </div>
+</template>
